c8983ae17ea95f47454d1ef2995ecaae25f6a8f5
Lela Smith <smith.lelaa@gmail.com>
2019-11-01 10:12:29 -0700

    begin back-end javascript logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
+//Back-end logic//
+function beepBoop(number) {
+  var newArray = []
+  for (var i = 0; i <= number; index ++) {
+
+
+  }
+}
+
+
+//Front-end logic//
 $(document).ready(function(){
   $("#beep-boop").submit(function(event){
     event.preventDefault();
 
-    var userNumber = $("input#numberInput").val();
     var userName = $("input#nameInput").val();
+    var userNumber = parseInt($("input#numberInput").val());
 
-    $("#result").append(userName)
+    $("#result").text(userName)
     $("#result").append(userNumber);
   });
 });
